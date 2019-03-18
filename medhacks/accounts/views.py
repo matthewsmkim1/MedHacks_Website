8623f55b0719d6876909565ad5eff0b81b01ad5f
Matt <Matthew.sm.kim1@gmail.com>
2019-03-18 17:14:40 -0500

    home view now renders from templates folder

--- medhacks/accounts/views.py
+++ medhacks/accounts/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Temp Home Page')
+    return render(request, 'accounts/login.html') #django automatically looks
+    #for a templates folder, that's why we make an additional accounts folder
