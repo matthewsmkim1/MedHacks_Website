3d43d2f70460799f0e8625b08ac47655577ac237
Matt <Matthew.sm.kim1@gmail.com>
2019-03-20 15:54:50 -0500

    added non functioning login page

--- medhacks/accounts/urls.py
+++ medhacks/accounts/urls.py
@@ -5,7 +5,8 @@ be in this file
 
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.home), #this means that accounts/ will render the home function in the views file in accounts
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login")
 ]
