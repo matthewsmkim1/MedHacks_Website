6448fd1559f53876cd7243051f9dcc7967c8453a
Matt <Matthew.sm.kim1@gmail.com>
2019-03-19 22:37:44 -0500

    added jinja templating and bootstrap link

--- medhacks/accounts/views.py
+++ medhacks/accounts/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/login.html') #django automatically looks
+    numbers = [1,2,3,4,5]
+    name = 'Matt kim'
+    args = {'Name': name, 'numbers': numbers}
+    return render(request, 'accounts/home.html', args) #django automatically looks
     #for a templates folder, that's why we make an additional accounts folder
+    #Third parameter in render is the data we want to pass through
