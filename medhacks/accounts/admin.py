9889e8a057f74663c6bf71a8bbeaba429a5079af
Matt <Matthew.sm.kim1@gmail.com>
2019-04-02 00:27:37 -0400

    admin view shows more fields

--- medhacks/accounts/admin.py
+++ medhacks/accounts/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from accounts.models import UserProfile #always have to import model
-# Register your models here.
 
+#Add class to add more columsn in admin view
+class UserProfileAdmin(admin.ModelAdmin):
+    #this tuple displays the ordering in the admin page as well
+    list_display = ('user', 'user_info', 'city', 'website', 'phone')
 
+    def user_info(self, obj):
+        return obj.description
+
+    #if we want to change the name of what appears in the django admin page
+    user_info.short_description = 'info'
 #This is display our model in the admin site
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
