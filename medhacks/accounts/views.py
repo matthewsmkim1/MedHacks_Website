c7c02a1ddd26b36243e11caf9ee5bf92204355d6
rguo123 <richardg7890@gmail.com>
2019-06-21 15:19:56 -0400

    removed verify link for email template

--- medhacks/accounts/views.py
+++ medhacks/accounts/views.py
@@ -38,12 +38,9 @@ def register(request):
 
             # format email information
             current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
+            mail_subject = 'Thank you for registering for medhacks!'
             message = render_to_string('accounts/acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-                'token':account_activation_token.make_token(user), # token from token generator
+                'user': user,                
             })
             to_email = form.cleaned_data.get('email')
             email = EmailMessage(mail_subject, message, to=[to_email])
